aca9991c76c195046d0e1fed31d8909ccef9b701
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-17 17:35:40 -0300

    adicionando mais eventos dos botoes e inputs

--- paginatabela JS/paginatabela.js
+++ paginatabela JS/paginatabela.js
@@ -1,14 +1,33 @@
 var botaoAdicionar = document.querySelector ('#adicionar-jogador');
 
-
-console.log (botaoAdicionar);
-
-    botaoAdicionar.addEventListener ('click' ,function () {
+    botaoAdicionar.addEventListener ('click' ,function (event) {
         event.preventDefault();
-        console.log ('clicado');
+        
+        var form = document.querySelector ('#form-adiciona');
+
+        var nome = form.nome.value;       
+        var vitorias = form.vitorias.value;
+        var  gols = form.gols.value;
+
+        var jogadorTr = document.createElement ("tr");
+
+        var nomeTd = document.createElement ("td");
+        var vitoriasTd = document.createElement ("td");
+        var golsTd = document.createElement ("td");
+
+
+        nomeTd.textContent = nome;
+        vitoriasTd.textContent =vitorias;
+        golsTd.textContent = gols;
+
+
+        jogadorTr = appendChild (nomeTd);
+        jogadorTr = appendChild (vitoriasTd);
+        jogadorTr = appendChild (golsTd);
+
     });
 
-
+    
 
     var botaoAdicionar = document.querySelector ('#apagar-jogador');
 
