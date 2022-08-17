3c2effb1f9dedaff759103ce21b0af14f1c49abe
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-17 17:54:58 -0300

    corrigindo bugs de input

--- paginatabela JS/paginatabela.js
+++ paginatabela JS/paginatabela.js
@@ -7,24 +7,32 @@ var botaoAdicionar = document.querySelector ('#adicionar-jogador');
 
         var nome = form.nome.value;       
         var vitorias = form.vitorias.value;
+        var pontosvitoria = form.pontosvitoria.value;
         var  gols = form.gols.value;
-
+     
         var jogadorTr = document.createElement ("tr");
 
         var nomeTd = document.createElement ("td");
         var vitoriasTd = document.createElement ("td");
+        var pontosvitoriaTd = document.createElement ("td");
         var golsTd = document.createElement ("td");
 
 
         nomeTd.textContent = nome;
-        vitoriasTd.textContent =vitorias;
+        vitoriasTd.textContent = vitorias;
+        pontosvitoriaTd.textContent = pontosvitoria;
         golsTd.textContent = gols;
 
 
-        jogadorTr = appendChild (nomeTd);
-        jogadorTr = appendChild (vitoriasTd);
-        jogadorTr = appendChild (golsTd);
+        jogadorTr.appendChild (nomeTd);
+        jogadorTr.appendChild (vitoriasTd);
+        jogadorTr.appendChild (pontosvitoriaTd);
+        jogadorTr.appendChild (golsTd);
 
+
+        var tabelaTBody = document.querySelector ('.tabela-jogador');
+
+        tabelaTBody.appendChild (jogadorTr);
     });
 
     
