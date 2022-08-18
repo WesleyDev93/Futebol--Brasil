bc3d9e307181039401aeb17d45dbc8d7d5e5662f
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-08-18 19:07:56 -0300

    Colocando estilos ao clicar nos botoes

---
+++ Js/trocaestilo.js
@@ -0,0 +1,66 @@
+// botoes
+
+var botaoFranca = document.querySelector ('.franca');
+
+var botaoPortugual = document.querySelector ('.portugual');
+
+
+var botaoBrasil = document.querySelector ('.brasil');
+
+// pagina
+
+var html = document.querySelector ('body');
+
+
+
+// estilos de imagem
+
+var mbappé = document.querySelector ('.alterarEstilo');
+
+var neymar = document.querySelector ('.principal__imagem');
+
+var cr7 = document.querySelector ('.alterarEstilo02');
+
+// frança
+
+botaoFranca.addEventListener ('click' ,function () {
+
+   html.classList.add ('body3');
+ 
+   mbappé.style.display = 'block';
+   cr7.style.display = 'none';
+
+   neymar.style.display = 'none';
+
+   
+});
+
+
+//portugual
+
+botaoPortugual.addEventListener ('click' ,function () {
+
+    html.classList.add ('body2');
+  
+    cr7.style.display = 'block';
+ 
+ 
+    neymar.style.display = 'none';
+    mbappé.style.display = 'none';
+    
+    
+ });
+
+// brasil
+
+ botaoBrasil.addEventListener ('click' ,function () {
+
+    html.classList.add ('body1');
+
+ 
+    neymar.style.display = 'block';
+    mbappé.style.display = 'none';
+    cr7.style.display = 'none';
+ 
+    
+ });
